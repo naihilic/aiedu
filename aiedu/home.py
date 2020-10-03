dcf35bcb72ceeeb2384f792eec5e6c34ddb31d57
naihilic <naihil@gmail.com>
2020-10-03 18:48:37 +0900

    data structure change

--- aiedu/home.py
+++ aiedu/home.py
@@ -2,45 +2,17 @@ import ipywidgets as widgets
 from IPython.display import display
 from ipywidgets import Label, HBox, VBox, HTML
 from IPython.display import YouTubeVideo
+import json
+
 # a talk about IPython at Sage Days at U. Washington, Seattle.
 # Video credit: William Stein.
 
-courses = { "프로그래밍": {
-              "Python": {
-                "Python 기초": { "desc": "blah" },
-                "Python 실습": { "desc": "blah" },
-                "Python 고급": { "desc": "blah" }
-              },
-              "Java": {},
-              "C/C++": {},
-              "Go": {}
-            },
-            "인공지능": {
-            },
-            "데이터분석": {
-            },
-            "클라우드": {
-              "도커": {},
-              "Kubernetes": {}
-            }
-}
+titles = ['1.과정선택', '2.동영상 강의', '3.요약정리', '4.테스트', '5.참고자료']
+courses = json.load(open("rc/courses.json"))
+#questions = json.load(open("rc/questions.json"))
+#videos = josn.load(open("rc/videos.json"))
 
-questions = { 'q1': {'type': 'select', 'question': '바람이 패배한 이유는?', 'choices': ['노력', '전략', '계절', '확률']},
-              'q2': {'type': 'select', 'question': '토끼가 간을 잃어버린 이유는?', 'choices': ['그런 일 없음', '실수', '건망증', '천재지변']},
-              'q3': {'type': 'select', 'question': '임베딩을 하는 이유는?', 'choices': ['재밌음', '멋있음', '신기함', '학습에 도움이 됨']},
-              'q4': {'type': 'select', 'question': '커피를 마셔야 하는 때는?', 'choices': ['배고플 때', '잠자기 전', '조깅 중', '좋은 날']}
-}
-
-courseCurr = { 'title': 'NA',
-                'videos': ['Nxz6FxGH_6U','',''],
-                'summaries': ['rc/a.html', 'rc/b.html'],
-                'questions': ['q1','q2','q3','q4'],
-                'references': ['r1.html', 'r2.html']
-             }
-
-videos = [ {'subject': '자전거를 배우자', 'vid': 'Nxz6FxGH_6U'},
-           {'subject': '노래를 배우자', 'vid': '2D5cmNyd0R0'},
-           {'subject': '우체국에 가보자', 'vid': 'dIY6y5f98qk'}]
+courseCurr = courses['프로그래밍']['Python']['Python 기초'];
 
 def on_summary_clicked(b):
     b.output.clear_output()
@@ -55,41 +27,56 @@ def on_summary_clicked(b):
 
 def on_test_clicked(b):
     b.output.clear_output()
+    courseCurr['questions'] = json.load(open(courseCurr['questionFile']))
     with b.output:
         courseCurr['idxQuestion'] = 0 if 'idxQuestion' not in courseCurr.keys() else courseCurr['idxQuestion']+(1 if b.action=='next' else -1)
         courseCurr['idxQuestion'] = 0 if courseCurr['idxQuestion'] <=0 else (len(courseCurr['questions'])-1 if courseCurr['idxQuestion']>=len(courseCurr['questions']) else courseCurr['idxQuestion'])
-        qid = courseCurr['questions'][courseCurr['idxQuestion']]
-        print(f'문제 : {questions[qid]["question"]}')
-        for idx, choice in enumerate(questions[qid]['choices']):
-
-
+        #qid = courseCurr['questions'][courseCurr['idxQuestion']]
+        print(f'문제 : {courseCurr["questions"][courseCurr["idxQuestion"]]["question"]}')
+        for idx, choice in enumerate(courseCurr["questions"][courseCurr["idxQuestion"]]['choices']):
             print(f'{idx}. {choice}')
 
-titles = ['1.과정선택', '2.동영상 강의', '3.요약정리', '4.테스트', '5.참고자료']
-
+def on_topicL1_change(change):
+    global courseCurr
+    outputDesc = courseCurr['outputDesc']
+    courseL1 = courses[change['new']]
+    courseL2 = courseL1[list(courseL1.keys())[0]]
+    courseCurr = courseL2[list(courseL2.keys())[0]]
+    courseCurr['outputDesc'] = outputDesc
+    outputDesc.clear_output()
+    with outputDesc:
+          display(HTML("<h1>간단소개</h1>"))
+          display(courseCurr['description'])
+            
 def init():
     tab = widgets.Tab(titles=['title:'+str(i) for i in range(len(titles))])
     tab.children = [widgets.Output() for title in titles]
     [tab.set_title(i, title) for i, title in enumerate(titles)]
 
     with tab.children[0]:
-      topicsL1 = widgets.Select( options=courses.keys(), value=list(courses.keys())[0], description='', disabled=False) # rows=10,  
+      courseCurr['outputDesc'] = widgets.Output()
+      topicsL1 = widgets.Select( options=courses.keys(), value=list(courses.keys())[0], description='', disabled=False) # rows=10,
+      topicsL1.observe(on_topicL1_change, names='value')
       topicsL2 = widgets.Select( options=courses[list(courses.keys())[0]].keys(), value=list(courses[list(courses.keys())[0]].keys())[0], description='', disabled=False) # rows=10,  
-      topicsL3 = widgets.Select( options=['Foo', 'Bar', 'Blah'], value='Foo', description='', disabled=False) # rows=10,  
+      topicsL3 = widgets.Select( options=['Foo', 'Bar', 'Blah'], value='Foo', description='', disabled=False) # rows=10,
+      with courseCurr['outputDesc']:
+          display(HTML("<h1>간단소개</h1>"))
+          display(courseCurr['description'])
       display(HBox([VBox([Label(value='대분류'), topicsL1]),
                     VBox([Label(value='중분류'), topicsL2]),
                     VBox([Label(value='소분류'), topicsL3])]))
+      display(courseCurr['outputDesc'])
     with tab.children[1]:
       labelCourse = Label(value='과정명: '+courseCurr['title'])
       display(labelCourse)
 
-      outputVideos = [widgets.Output() for video in videos]
+      outputVideos = [widgets.Output() for video in courseCurr['videos']]
       for idx, outputVideo in enumerate(outputVideos):
         with outputVideo:
-            display(YouTubeVideo(videos[idx]['vid']))
+            display(YouTubeVideo(courseCurr['videos'][idx]['vid']))
       #[videos[idx].display(YouTubeVideo(vid)) for idx, vid in enumerate(vids)]
       accordion = widgets.Accordion(children=outputVideos)
-      [accordion.set_title(idx, video['subject']) for idx, video in enumerate(videos)]
+      [accordion.set_title(idx, video['subject']) for idx, video in enumerate(courseCurr['videos'])]
       display(accordion)
       #print('아코디언 형태로 비디오 목록 보여주면 좋을듯...')
       #display(YouTubeVideo('Nxz6FxGH_6U'))
@@ -97,6 +84,8 @@ def init():
     with tab.children[2]:
       labelCourse = Label(value='과정명: '+courseCurr['title'])
       outputContent = widgets.Output()
+      with outputContent:
+          display("[다음]버튼을 눌러서 시작해주세요.")
       btnPrev = widgets.Button(description='이전')
       btnPrev.action = 'prev'
       btnPrev.output = outputContent#tab.children[2]
@@ -110,6 +99,8 @@ def init():
     with tab.children[3]:
       labelCourse = Label(value='과정명: '+courseCurr['title'])
       outputContent = widgets.Output()
+      with outputContent:
+          display("[다음]버튼을 눌러서 시작해주세요.")
       btnPrev = widgets.Button(description='이전')
       btnPrev.action = 'prev'
       btnPrev.output = outputContent#tab.children[2]
