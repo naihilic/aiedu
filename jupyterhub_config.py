cc73430739e98866ea9403afd004eee35d8f4e13
naihilic <42593479+naihilic@users.noreply.github.com>
2020-10-13 11:14:31 +0900

    Create jupyterhub_config.py

---
+++ jupyterhub_config.py
@@ -0,0 +1,5 @@
+c.JupyterHub.tornado_settings = {                                                            
+  'headers': {                                                                               
+    'Content-Security-Policy': "frame-ancestors 'self' http://d4d5db0aad48.ngrok.io"                     
+  }                                                                                          
+}                                                                                            
