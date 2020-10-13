b07ea02d14395bc4313818874bfca501094f449a
naihilic <42593479+naihilic@users.noreply.github.com>
2020-10-13 11:52:53 +0900

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 c.NotebookApp.tornado_settings = {
     'headers': {
-        'Content-Security-Policy': "frame-ancestors 'self' http://d4d5db0aad48.ngrok.io "
+        'Content-Security-Policy': 'frame-ancestors self http://d4d5db0aad48.ngrok.io',
     }
 }
 
