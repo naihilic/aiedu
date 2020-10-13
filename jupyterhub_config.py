0022a398d7b46c0662c46bce2af943685d2d9521
naihilic <42593479+naihilic@users.noreply.github.com>
2020-10-13 23:39:17 +0900

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,3 +1,4 @@
+c.Spawner.args = ["--NotebookApp.tornado_settings={ 'headers': { 'Content-Security-Policy': 'frame-ancestors * self http://efe1169870e5.ngrok.io http://c3ff4430fcf1.ngrok.io http://a575592b5e57.ngrok.io'} }"]  
 c.JupyterHub.tornado_settings = { 
   'headers': { 
     'Content-Security-Policy': 'frame-ancestors self http://efe1169870e5.ngrok.io http://c3ff4430fcf1.ngrok.io http://a575592b5e57.ngrok.io' 
