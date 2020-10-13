11137c89f478fc9ef871fc46e07613bcf19c68be
naihilic <42593479+naihilic@users.noreply.github.com>
2020-10-13 13:06:09 +0900

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,5 +1,5 @@
 c.JupyterHub.tornado_settings = {                                                            
   'headers': {                                                                               
-    'Content-Security-Policy': 'frame-ancestors self http://d4d5db0aad48.ngrok.io',                     
+    'Content-Security-Policy': 'frame-ancestors self http://d4d5db0aad48.ngrok.io http://6c39de9e39ea.ngrok.io',                     
   }                                                                                          
 }                                                                                            
