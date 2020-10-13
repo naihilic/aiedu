f3b4e6976896052f3d9fa7811bcae8c00c4d11a6
naihilic <42593479+naihilic@users.noreply.github.com>
2020-10-13 20:17:25 +0900

    Update and rename jupyter_notebook_config.py__ to jupyter_notebook_config.py

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.tornado_settings = {
+    'headers': {
+        'Content-Security-Policy': "frame-ancestors 'self' 'efe1169870e5.ngrok.io' 'c3ff4430fcf1.ngrok.io' 'a575592b5e57.ngrok.io'",
+    }
+}
+
+
