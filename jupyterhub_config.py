315ee1bc75ae25d58aa6b3f76f13608dbb69003e
naihilic <42593479+naihilic@users.noreply.github.com>
2020-10-13 18:19:02 +0900

    Update and rename jupyterhub_config.py__ to jupyterhub_config.py

---
+++ jupyterhub_config.py
@@ -0,0 +1,5 @@
+c.JupyterHub.tornado_settings = { 
+  'headers': { 
+    'Content-Security-Policy': "frame-ancestors 'self' 'efe1169870e5.ngrok.io' 'c3ff4430fcf1.ngrok.io' 'a575592b5e57.ngrok.io'" 
+  } 
+} 
