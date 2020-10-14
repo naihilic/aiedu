e984cc5432bdbe0e4bde48c855b78f181021ecb6
naihilic <42593479+naihilic@users.noreply.github.com>
2020-10-14 20:13:44 +0900

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -3,9 +3,10 @@ c.JupyterHub.tornado_settings = {
   'headers': { 
     'Content-Security-Policy': "frame-ancestors http://46d2446d1b0b.ngrok.io 'self'"
   },
-  'xsrf_cookie_kwargs': {'SameSite': "None", 'Secure': True},
-  'cookie_options': {'SameSite': "None", 'Secure': True}
+#  'xsrf_cookie_kwargs': {'SameSite': "None", 'Secure': True},
+#  'cookie_options': {'SameSite': "None", 'Secure': True}
 }
-c.JupyterHub.disable_check_xsrf = True
+c.JupyterHub.disable_check_xsrfBool = True
+#c.JupyterHub.disable_check_xsrf = True
 #c.Spawner.args = [ '--config=/home/jovyan/jupyter_notebook_config.py']
 #c.JupyterHub.tornado_settings = {"cookie_options": {"SameSite": "None", "Secure": True}}
